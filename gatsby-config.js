c1cd706585092539142d7fbd8cf659a3de13ead5
Carlos Garcia Martin <carlos@carlosgm.com>
2020-10-25 08:49:02 +0000

    feature: personalise manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,13 +60,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `carlosgm`,
+        short_name: `carlosgm`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#D84B39`,
+        theme_color: `#D84B39`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/carlosgm-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
